1b5023dc01bc21dd450c135799a9500c14ebd381
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2024-12-27 13:21:18 +0200

    Solve day 25

---
+++ src/25/day25.ts
@@ -0,0 +1,9 @@
+import {type Input} from './day25.input.js';
+
+export const solvePart1 = (input: Input): number => {
+  return 0;
+};
+
+export const solvePart2 = (input: Input): number => {
+  return 0;
+};
